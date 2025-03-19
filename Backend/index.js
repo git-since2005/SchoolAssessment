e690df2aad2165fe61aecf99a216e4d37cade0d5
git-since2005 <84366921+git-since2005@users.noreply.github.com>
2025-03-19 21:57:08 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 
 const app = express();
 app.use(express.json());
-app.use(cors({"origin":"https://school-assessment.netlify.app/"}))
+app.use(cors({"origin":"https://school-assessment.netlify.app"}))
 
 // MySQL Connection
 const pool = mysql.createPool({
